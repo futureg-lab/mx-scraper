9f542eb6eb5b7591ce4888ca08d5d3421bf85f80
Michaël <afmichael73@gmail.com>
2022-10-21 01:17:47 +0300

    fix : edgecase on content layout

--- src/sandbox/down-test.ts
+++ src/sandbox/down-test.ts
@@ -1,7 +1,7 @@
 import { CustomRequest } from "../utils/CustomRequest";
 
 const request = new CustomRequest ();
-const test_url = 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg';
+const test_url = 'https://blogger.googleusercontent.com/img/a/AVvXsEgwyiY-kBKczKKNZ2nWKGIuL-O2vi_du_7XhSSKGI_eDZfzWUNdX24kQPeJoOYM1pAmH1KaBHjJOcukSQz-Fsh2aI2v8wo9lJO9BpGiEJV_r8bgqpci2Mgs8hHPtHssBfwKk_eKwcxsvsxOmhVx3Q5J-FRT6_DAFjh0XcPlTuMrD7L6KWxtm-TYraPG';
 request
     .download (test_url, './download/temp/Cat03.jpg')
     .then (() => {
